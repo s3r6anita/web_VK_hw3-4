397a79f82a22e50cd2d86441e7c7d4aa2c438470
s3r6anita <xz200359@gmail.com>
2023-11-15 23:45:40 +0300

    repeat last commit

--- askRakhmanov/settings.py
+++ askRakhmanov/settings.py
@@ -54,9 +54,7 @@ ROOT_URLCONF = 'askRakhmanov.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates'
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,14 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
