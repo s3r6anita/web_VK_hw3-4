1e1969f1d6fca17b9b005a4bef546a6a659d2d4b
s3r6anita <xz200359@gmail.com>
2023-11-16 13:02:57 +0300

    Revert "repeat last commit"
    This reverts commit 397a79f82a22e50cd2d86441e7c7d4aa2c438470.

--- askRakhmanov/urls.py
+++ askRakhmanov/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('hot', views.hot, name="hot"),
+    path('tag/<tag_name>', views.tag, name="tag"),
+    path('questions/<int:question_id>', views.question, name="question"),
+    path('login', views.login, name="login"),
+    path('signup', views.signup, name="signup"),
+    path('ask', views.ask, name="ask"),
+    path('settings', views.settings, name="settings"),
 ]
